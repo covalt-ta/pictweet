c6cb62547097dd33dac192b86379e75864f4d2f7
covalt-ta <taku.covayashi@gmail.com>
2020-07-22 20:36:26 +0900

    SQL言語による取得の確認

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,6 +4,8 @@ class TweetsController < ApplicationController
 
   def index
     @tweets = Tweet.includes(:user).order("created_at DESC")
+    # query = "SELECT * FROM tweets"
+    # @tweets = Tweet.find_by_sql(query)
   end
 
   def show
