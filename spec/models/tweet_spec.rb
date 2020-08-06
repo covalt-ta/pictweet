5fa412cfdaa0181de15bc5584414404d2fb308ef
covalt-ta <taku.covayashi@gmail.com>
2020-08-06 20:04:46 +0900

    日本語化による単体テストの記述変更

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -8,21 +8,21 @@ RSpec.describe Tweet, type: :model do
       it "画像とテキストがあればツイートは保存できる" do
         expect(@tweet).to be_valid
       end
-      it "テキストのみあれば保存できる" do
-        @tweet.image = ""
-        expect(@tweet).to be_valid
-      end
+      # it "テキストのみあれば保存できる" do
+      #   @tweet.image = ""
+      #   expect(@tweet).to be_valid
+      # end
     end
     context 'ツイートが保存できない場合' do
       it 'テキストがないと保存できない' do
         @tweet.text = ""
         @tweet.valid?
-        expect(@tweet.errors.full_messages).to include("Text can't be blank")
+        expect(@tweet.errors.full_messages).to include("テキストを入力してください")
       end
       it 'ユーザーが紐付いてないと保存できない' do
         @tweet.user = nil
         @tweet.valid?
-        expect(@tweet.errors.full_messages).to include("")
+        expect(@tweet.errors.full_messages).to include("Userを入力してください")
       end
     end
   end
